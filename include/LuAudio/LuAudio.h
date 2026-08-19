e6d23e4d34661cc653a8e101ae7c1386a0beab79
Yuki <kazolikescats@gmail.com>
2026-08-19 13:33:38 +0300

    Feat: Initial commit

---
+++ include/LuAudio/LuAudio.h
@@ -0,0 +1,5 @@
+﻿// Only add header files that never change in here
+
+#pragma once
+
+#include <iostream>
---
+++ src/Main.cpp
@@ -0,0 +1,9 @@
+﻿#include <LuAudio/LuAudio.h>
+
+using namespace std;
+
+int main()
+{
+	cout << "Hello CMake." << endl;
+	return 0;
+}
